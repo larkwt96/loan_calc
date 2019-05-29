1fa8ef4257fa4f91b7abc146e7c61237fb35879b
Lucas Wilson <lkwilson96@gmail.com>
2019-05-28 22:26:39 -0600

    build loan unit component

---
+++ src/ui/input/Loan.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Loan extends React.Component {
+  render() {
+    const { loan_amount, loan_rate, updateLoanAmount, updateLoanRate } = this.props;
+    return (
+      <React.Fragment>
+        <div className="form-group">
+          <label>Loan Balance</label>
+          <div className="input-group mb-3 col-xl-8 col-lg-10">
+            <div className="input-group-prepend">
+              <span className="input-group-text">$</span>
+            </div>
+            <input type="number" step=".01" className="form-control"
+              placeholder="0" value={loan_amount}
+              onChange={(event) => updateLoanAmount(event.currentTarget.value)} />
+          </div>
+          {/*<small className="form-text text-muted">Input loan amount (in dollars).</small>*/}
+        </div>
+
+        <div className="form-group">
+          <label>Loan Interest Rate (%)</label>
+          <div className="input-group mb-3 col-xl-8 col-lg-10">
+            <input type="number" step=".01" className="form-control"
+              placeholder="0" value={loan_rate}
+              onChange={(event) => updateLoanRate(event.currentTarget.value)} />
+            <div className="input-group-append">
+              <span className="input-group-text">%</span>
+            </div>
+          </div>
+          {/*<small className="form-text text-muted">Input loan rate (in percentage).</small>*/}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Loan;
--- src/ui/input/Loans.js
+++ src/ui/input/Loans.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import Loan from './Loan';
 
 class Loans extends React.Component {
-
   updateLoanRate(loan_rate) {
     if (loan_rate === "") {
       loan_rate = undefined;
@@ -18,33 +18,11 @@ class Loans extends React.Component {
   render() {
     const { loan_amount, loan_rate } = this.props;
     return (
-      <React.Fragment>
-        <div className="form-group">
-          <label>Loan Balance</label>
-          <div className="input-group mb-3 col-xl-8 col-lg-10">
-            <div className="input-group-prepend">
-              <span className="input-group-text">$</span>
-            </div>
-            <input type="number" step=".01" className="form-control"
-              placeholder="0" value={loan_amount}
-              onChange={(event) => this.updateLoanAmount(event.currentTarget.value)} />
-          </div>
-          {/*<small className="form-text text-muted">Input loan amount (in dollars).</small>*/}
-        </div>
-
-        <div className="form-group">
-          <label>Loan Interest Rate (%)</label>
-          <div className="input-group mb-3 col-xl-8 col-lg-10">
-            <input type="number" step=".01" className="form-control"
-              placeholder="0" value={loan_rate}
-              onChange={(event) => this.updateLoanRate(event.currentTarget.value)} />
-            <div className="input-group-append">
-              <span className="input-group-text">%</span>
-            </div>
-          </div>
-          {/*<small className="form-text text-muted">Input loan rate (in percentage).</small>*/}
-        </div>
-      </React.Fragment>
+      <Loan
+        loan_amount={loan_amount}
+        loan_rate={loan_rate}
+        updateLoanAmount={loan_amount => this.updateLoanAmount(loan_amount)}
+        updateLoanRate={loan_rate => this.updateLoanRate(loan_rate)} />
     );
   }
 }
